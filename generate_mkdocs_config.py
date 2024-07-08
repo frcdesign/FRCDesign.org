7bb601fac65a6194eb2b272465838432c161bdf3
Revision0 <groose876@gmail.com>
2024-07-08 13:03:08 -0700

    Added cards to the mechanism examples index page
    added cards, added emoji/icon capabilities, edited the python file to account for the custom tags.

--- generate_mkdocs_config.py
+++ generate_mkdocs_config.py
@@ -1,11 +1,24 @@
 import yaml
 import os
+from material.extensions import emoji
 
 new_properties = {'repository': 'davidsdesignserver/FRCDesign.org', 'token': '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', 'enabled': True}
 
-# Load the base configuration
-with open('mkdocs.base.yml', 'r') as file:
-    config = yaml.safe_load(file)
+# Function to load YAML safely
+def load_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return yaml.load(file, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML in file '{file_path}':")
+        print(e)
+        return None
+
+# Load the base configuration using safe loader
+config = load_yaml('mkdocs.base.yml')
 
 # Function to add or update properties
 def update_plugin_properties(plugins, plugin_name, new_properties):
@@ -33,8 +46,7 @@ if branch == 'main':
 
 # Write the modified configuration to mkdocs.yml
 with open('mkdocs.yml', 'w') as file:
-    yaml.safe_dump(config, file)
+    yaml.dump(config, file)
 
-print(config)
+print(config['plugins'])
 print(f"Generated mkdocs.yml for branch: {branch}")
-print(os.getenv('GITHUB_REF'))
