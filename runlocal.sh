f6540fd07425539e17e5ce3b7634dbf0de27abe0
Revision0 <groose876@gmail.com>
2024-07-09 16:44:39 -0700

    added separate dependency script, edited readme, added insiders env variable

--- runlocal.sh
+++ runlocal.sh
@@ -1,9 +1,2 @@
 source venv/scripts/activate 
-pip install git+https://${INSIDERS}@github.com/squidfunk/mkdocs-material-insiders.git
-pip install -e mkdocs-material
-pip install mkdocs-glightbox
-pip install mkdocs-git-revision-date-localized-plugin
-pip install pymdown-extensions pyyaml
-pip install mkdocs-git-committers-plugin-2
-pip install "mkdocs-material[imaging]"
 mkdocs serve
