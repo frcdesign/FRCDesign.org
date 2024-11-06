f52545d960dff41f601c856ef24074d638ebd051
Andrew Card <andrewccard04@gmail.com>
2024-11-05 18:36:54 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip install mkdocs-git-revision-date-localized-plugin
 pip install pymdown-extensions pyyaml
 pip install mkdocs-git-committers-plugin-2
 pip install "mkdocs-material[imaging]"
+mkdocs serve
