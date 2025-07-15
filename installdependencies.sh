e8a3a6248c5a744af63137e59f39512ff995cfab
Siddharth Rao <siddrao.926@gmail.com>
2025-07-14 19:45:57 -0700

    Added PDM

--- installdependencies.sh
+++
@@ -1,7 +0,0 @@
-source venv/scripts/activate
-pip install mkdocs-material
-pip install mkdocs-glightbox
-pip install mkdocs-git-revision-date-localized-plugin
-pip install pymdown-extensions pyyaml
-pip install mkdocs-git-committers-plugin-2
-pip install "mkdocs-material[imaging]"
