51c5ecb6572e8bfc4e5c00589915365b6dabe9ca
Andrew Card <andrewccard04@gmail.com>
2025-07-30 10:46:54 -0700

    changes

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-source ./venv/bin/activate
+source ./venv/scripts/activate
 mkdocs serve
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 python -m venv ./venv/
-source ./venv/bin/activate
+source ./venv/scripts/activate
 pip install -r requirements.txt
